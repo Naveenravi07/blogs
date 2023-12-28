f1b2c0538102f42839fbd82e8c2f8edfa36c99f6
Naveenravi07 <gamerkidnav@gmail.com>
2023-12-28 13:52:57 +0530

    Site rebuild

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 zola build
-rsync -i "~/servers/personal_mumbai.pem" -R --delete /var/www/blogs/* admin@3.109.70.6:/var/www/blogs
+rsync -i "~/servers/personal_mumbai.pem" -arP --delete /var/www/blogs/* admin@3.109.70.6:/var/www/blogs
 git add .
 git commit -m "Site rebuild"
 git push
